212c06f2269f48a7ed6e5d775305116ae874726a
andydna <andydna@protonmail.ch>
2020-05-12 14:53:29 -0500

    spec

--- spec/TitleWave_spec.rb
+++ spec/TitleWave_spec.rb
@@ -1,8 +1,10 @@
 require 'open-uri'
 
 RSpec.describe TitleWave do
-  specify "VCR just works", :vcr do
-    page = URI.open("https://rubygems.org").read
-    expect(`find . -name '*just*works*'`).to match /yml/
+  context "this should fail the first time" do
+    specify "VCR just works", :vcr do
+      page = URI.open("https://rubygems.org").read
+      expect(`find . -name '*just*works*'`).to match /yml/
+    end
   end
 end
